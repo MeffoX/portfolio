930e6afd4d9a0b048ba0e3a7756e50f08620542c
Stefan <roth.neukirchen@googlemail.com>
2023-09-05 11:30:19 +0200

    navbar finish

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,10 +7,28 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   isMenuOpen: boolean = false;
+  isCopied: boolean = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
     document.body.style.overflow = this.isMenuOpen ? 'hidden' : 'auto';
   }
+
+
+  copyToClipboard(element: HTMLParagraphElement) {
+    const textToCopy = element.innerHTML;
+    
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      console.log('Text erfolgreich in die Zwischenablage kopiert.');
+    }).catch(err => {
+      console.error('Fehler beim Kopieren in die Zwischenablage: ', err);
+    });
+  
+    this.isCopied = true;
+    setTimeout(() => {
+      this.isCopied = false;
+    }, 3000);
+  }
+  
 
 }
