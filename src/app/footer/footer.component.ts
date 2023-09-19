b694f8cc353e0d9c7fb71bf385d639fa94f13baf
Stefan <roth.neukirchen@googlemail.com>
2023-09-19 13:52:05 +0200

    responsive startscreen

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScrollService } from '../scroll.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+  constructor(private scrollService:ScrollService){}
+
+
+  scrollTo(section:string) {
+    this.scrollService.scrollToElement(section);
+}
 
 }
